080d388db509c6befc66f1adf903fdb6e3791bf2
PatrickRingbauer <36598027+PatrickRingbauer@users.noreply.github.com>
2018-02-18 17:41:37 +0100

    Fehlertext und weitere Wetterinfos

--- Mirrorsite/JavaScript.js
+++ Mirrorsite/JavaScript.js
@@ -3,31 +3,43 @@
 $(document).ready(function () {
     console.log("1");
     gettingJSON();
+    startTime();
+
     setInterval(function () {
         console.log("6 - in interval");                    // wird jede 10 Minuten aufgerufen
         gettingJSON();
     }, 600000);
 
-    setInterval(function () {
+    setInterval(function () {                               // wird jede 0,5 Sekunden aufgerufen
         startTime();
     }, 500);
 });
 
 function gettingJSON() {
     console.log("2 - getting JSON start");
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=2862831&APPID=00c5a9bacda1d265d5ad4692edf1acaf", function (json, status) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=2862831&APPID=00c5a9bacda1d265d5ad4692edf1acaf", function (json, status) {             // holt die JSON
         console.log('4 - in "getJSON" callback function')
         console.log("status: " + status);
+
+        if (status =! "success") {
+            document.getElementById('success').innerHTML = "Fehler beim Wetterdatenabruf"
+        }
         myjson = json;
         updateUI();
-    });    // holt die JSON
+    });    
 
     console.log("3 - Zeile nach getJSON()");
 }
 
 function updateUI() {
     console.log("5 - in updateUI");
-    document.getElementById('temp').innerHTML = myjson.main.temp;
+    document.getElementById('city').innerHTML = myjson.name;
+    document.getElementById('temp').innerHTML = "Temperatur " + (myjson.main.temp - 273.15).toFixed(1) + " °C";
+    document.getElementById('humi').innerHTML = "Luftfeuchtigkeit " + myjson.main.humidity + " %";
+    document.getElementById('windspeed').innerHTML = "Windgeschwindigkeit " + myjson.wind.speed + " km/h";
+    //document.getElementById('beschreibung').innerHTML = myjson.weather.description;
+    console.log(myjson.weather.description);
+    console.log(myjson.weather.main);
 }
 
 function startTime() {
@@ -37,9 +49,7 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML =
-        h + ":" + m + ":" + s;
-
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
 }
 function checkTime(i) {
     if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
